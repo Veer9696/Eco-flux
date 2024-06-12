37dbc96c8f9cbca8abc1484d2b89c01a8e2404ad
Veer9696 <165047471+Veer9696@users.noreply.github.com>
2024-06-13 02:40:55 +0530

    Add files via upload

---
+++ EcoFluxlang-bot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABotWithMSI.sln -p:DeployOnBuild=true -p:PublishProfile=.\EcoFluxlang-bot-864f-Web-Deploy.pubxml -p:Password=awfzlQ8bvhGrFc0HQnWFaZaRnfFgtHdDLjrvyB852NJAQAkbyzcqot2rjrj8
+
